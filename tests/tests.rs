90ad8194e159be25d95ed80429229a97af8a15f2
bidhan-a <bidhan619@gmail.com>
2026-05-03 23:09:37 +0545

    fix: tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -16,7 +16,9 @@ use sbpf_assembler::{
     parser::Token,
 };
 use sbpf_common::{
-    inst_param::Number, instruction::{AsmFormat, Instruction}, opcode::Opcode,
+    inst_param::Number,
+    instruction::{AsmFormat, Instruction},
+    opcode::Opcode,
 };
 use sbpf_linker::byteparser::parse_bytecode;
 
@@ -191,7 +193,10 @@ fn render_instruction(
     {
         let target = offset as i64 + 8 + value * 8;
         if let Some(label) = code_labels.get(&target) {
-            return Ok(vec![instruction.to_asm(AsmFormat::Default)?, format!("call {label}")]);
+            return Ok(vec![
+                instruction.to_asm(AsmFormat::Default)?,
+                format!("call {label}"),
+            ]);
         }
     }
 
